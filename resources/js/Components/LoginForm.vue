b7cc2423085d3c60c234dbe0384a251f87a836b8
Jason Bogle <kicker10bog@gmail.com>
2025-01-26 12:59:45 -0600

    minor changes

--- resources/js/Components/LoginForm.vue
+++ resources/js/Components/LoginForm.vue
@@ -3,7 +3,7 @@ import BasicButton from '@/Components/BasicButton.vue';
 import Checkbox from '@/Components/Checkbox.vue';
 import InputError from '@/Components/InputError.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { Link, useForm } from '@inertiajs/vue3';
+import { useForm } from '@inertiajs/vue3';
 
 defineProps({
   canResetPassword: {
